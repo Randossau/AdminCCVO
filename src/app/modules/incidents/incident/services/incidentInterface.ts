a88736d683be11712de21762ce6ebefcf5b053b3
Marie <marie.morzadec@gmail.com>
2020-04-26 18:08:14 +0200

    ajout service incident + corrections beug

---
+++ src/app/modules/incidents/incident/services/incidentInterface.ts
@@ -0,0 +1,11 @@
+export interface IncidentsI {
+
+    id: string;
+    date: Date;
+    description: string;
+    email: string;
+    image: string;
+    libelle: string;
+    lieu: string;
+    type: string;
+}
--- src/app/modules/incidents/incidents.component.ts
+++ src/app/modules/incidents/incidents.component.ts
@@ -5,28 +5,30 @@ import { MatSort } from '@angular/material/sort';
 import { MatDialogConfig } from '@angular/material/dialog/dialog-config';
 import { MatDialog } from '@angular/material/dialog';
 import { IncidentComponent } from './incident/incident.component';
+import { IncidentsI } from './incident/services/incidentInterface';
+import { IncidentServService } from './incident/services/incident-serv.service';
 
-export interface Incidents {
-  no: number;
-  libelle: string;
-  type: string;
-  lieu: string;
-  date_incident: string;
-  user: string;
+// export interface Incidents {
+//   no: number;
+//   libelle: string;
+//   type: string;
+//   lieu: string;
+//   date_incident: string;
+//   user: string;
 
-}
+// }
 
-const DATA: Incidents[] = [
-  {no: 1, libelle: 'Problème', type: 'Sentier impraticable', lieu: 'trois croix', date_incident: '04/02/2020', user: '04/03/2020',},
-  {no: 2, libelle: 'Nid de guêpes', type: 'Animaux/Insecte', lieu: 'fontaine esteret', date_incident: '04/02/2020', user: '04/03/2020',},
-  {no: 3, libelle: 'Route barrée', type: 'Sentier impraticable', lieu: 'jurade', date_incident: '04/02/2020', user: '04/03/2020',},
-  {no: 4, libelle: 'Panneau illisible', type: 'Balisage', lieu: 'cinq monts', date_incident: '02/02/2020', user: '04/03/2020',},
-  {no: 5, libelle: 'Troupeau', type: 'Animaux/Insecte', lieu: 'lac du lurien', date_incident: '04/02/2020', user: '04/03/2020',},
-  {no: 6, libelle: 'Inondations', type: 'Sentier impraticable', lieu: 'larroun', date_incident: '08/02/2020', user: '04/03/2020',},
-  {no: 7, libelle: 'Arbre mort', type: 'Végétation gênante', lieu: 'trois croix', date_incident: '04/02/2020', user: '04/03/2020',},
-  {no: 8, libelle: 'Ronces', type: 'Végétation gênante', lieu: 'piemont du rey', date_incident: '15/02/2020', user: '04/03/2020',},
-  {no: 9, libelle: 'Arbre tombé', type: 'Sentier impraticable', lieu: 'artourste', date_incident: '06/02/2020', user: '04/03/2020',},
-];
+// const DATA: Incidents[] = [
+//   {no: 1, libelle: 'Problème', type: 'Sentier impraticable', lieu: 'trois croix', date_incident: '04/02/2020', user: '04/03/2020',},
+//   {no: 2, libelle: 'Nid de guêpes', type: 'Animaux/Insecte', lieu: 'fontaine esteret', date_incident: '04/02/2020', user: '04/03/2020',},
+//   {no: 3, libelle: 'Route barrée', type: 'Sentier impraticable', lieu: 'jurade', date_incident: '04/02/2020', user: '04/03/2020',},
+//   {no: 4, libelle: 'Panneau illisible', type: 'Balisage', lieu: 'cinq monts', date_incident: '02/02/2020', user: '04/03/2020',},
+//   {no: 5, libelle: 'Troupeau', type: 'Animaux/Insecte', lieu: 'lac du lurien', date_incident: '04/02/2020', user: '04/03/2020',},
+//   {no: 6, libelle: 'Inondations', type: 'Sentier impraticable', lieu: 'larroun', date_incident: '08/02/2020', user: '04/03/2020',},
+//   {no: 7, libelle: 'Arbre mort', type: 'Végétation gênante', lieu: 'trois croix', date_incident: '04/02/2020', user: '04/03/2020',},
+//   {no: 8, libelle: 'Ronces', type: 'Végétation gênante', lieu: 'piemont du rey', date_incident: '15/02/2020', user: '04/03/2020',},
+//   {no: 9, libelle: 'Arbre tombé', type: 'Sentier impraticable', lieu: 'artourste', date_incident: '06/02/2020', user: '04/03/2020',},
+// ];
 
 
 @Component({
@@ -36,16 +38,32 @@ const DATA: Incidents[] = [
 })
 
 export class IncidentsComponent implements OnInit {
-  displayedColumns: string[] = ['no', 'nom', 'prenom', 'email', 'date_creation', 'derniere_connexion','actions'];
-  dataSource = new MatTableDataSource<Incidents>(DATA);
+
+  incident: IncidentsI[];
+
+  displayedColumns: string[] = ['no','date', 'description', 'email','image','libelle', 'lieu','type', 'statut'];
+  dataSource = new MatTableDataSource<IncidentsI>(this.incident)
+
+
+  // displayedColumns: string[] = ['no', 'nom', 'prenom', 'email', 'date_creation', 'derniere_connexion','actions'];
+  // dataSource = new MatTableDataSource<IncidentsI[]>(incident);
+
+  // incident: IncidentsI[];
 
   @ViewChild(MatSort, {static: true}) sort: MatSort;
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   constructor(
-    private dialog: MatDialog) { }
+    private dialog: MatDialog, 
+    private incidentServ: IncidentServService) { }
 
   ngOnInit(): void {
+
+    this.incidentServ.getIncident().subscribe(incidents => {
+      console.log(incidents);
+      this.incident = incidents;
+    });
+    
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
